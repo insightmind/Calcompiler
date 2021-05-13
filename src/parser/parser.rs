fabedeffc97357ab2ea93747875845877b9a24dd
Niklas Bülow <me@niklasbuelow.com>
2021-05-13 02:59:09 +0200

    Add comment about parser type

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -2,12 +2,13 @@ use crate::lex::token::Token;
 
 // The parser should verify following grammar and create a AST for it:
 //
-// E = T, E';
-// E'= +TE', -TE', "";
+// E = T | E';
+// E'= +TE' | -TE' | "";
 // T = FT';
-// T'= *FT', /FT', "";
-// F = (E), number
+// T'= *FT' | /FT' | "";
+// F = (E) | number
 //
+// An SLL(1) parser should do the job here.
 #[allow(dead_code)]
 pub struct Parser {
     pub token_stream: Vec<Token>
